851ce0e577cfa67f4cf21f622658ad17338360ae
gitalexhubuser <alexdubovyck@yandex.ru>
2023-09-05 03:59:34 +0300

    + if __name__ == "__main__":

--- main.py
+++ main.py
@@ -3,17 +3,24 @@ from Libs.pycolors2 import PyColors
 
 colors = PyColors() # Создаем экземпляр класса PyColors
 
+def offlineParser(filePath):
+    with open(filePath, encoding="utf8", errors='ignore') as file:
+        source = file.read()
+    # print(f"source: {colors.YELLOW}{source}{colors.ENDC}")
+
+    soup = BeautifulSoup(source, "lxml")
+
+    title = soup.title.string
+    title1 = soup.title.text
+    # print(f"title: {colors.YELLOW}{title}{colors.ENDC}")
+    # colors.yellow_print(title)
+
+    colors.yellow_print("title", title)
+    colors.mass_print("title", title, title1)
+
+    # .find() .find_all()
+
+
 # offline
-with open("./_Target/index.html", encoding="utf8", errors='ignore') as file:
-    source = file.read()
-# print(f"source: {colors.YELLOW}{source}{colors.ENDC}")
-
-soup = BeautifulSoup(source, "lxml")
-
-title = soup.title.string
-title1 = soup.title.text
-# print(f"title: {colors.YELLOW}{title}{colors.ENDC}")
-# colors.yellow_print(title)
-
-colors.yellow_print("title", title)
-colors.mass_print("title", title, title1)
+if __name__ == "__main__":
+    offlineParser("./_Target/index.html")
