1d667a9be6ee14d97b93f9609de4f88622a3c253
gitalexhubuser <alexdubovyck@yandex.ru>
2023-09-05 03:35:44 +0300

    + requests + offline +online

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 from bs4 import BeautifulSoup
+import requests
+
+# online
+# https://sirus.su/base/character/57/9202/
+
+# offline
+with open("./_Target/index.html") as file:
+    source = file.read()
+    print("source: ", source)
+
+# soup = BeautifulSoup(source, "lxml")
