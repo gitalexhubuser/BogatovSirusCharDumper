739953835196f26d7c2a3565850543d776009a81
gitalexhubuser <alexdubovyck@yandex.ru>
2023-09-05 03:40:10 +0300

    + color +error utf8

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
+from Libs.pycolors import pycolors
+
+colors = pycolors() # Создаем экземпляр класса pycolors
 
 # online
 # https://sirus.su/base/character/57/9202/
 
 # offline
-with open("./_Target/index.html") as file:
+with open("./_Target/index.html", encoding="utf8", errors='ignore') as file:
     source = file.read()
-    print("source: ", source)
+    print(f"source: {colors.YELLOW}{source}{colors.ENDC}")
 
 # soup = BeautifulSoup(source, "lxml")
